5697f17a6f22d22d8a3ee66f21c92e0a29a5b53c
SciLor <SciLor@github.com>
2023-03-22 18:56:50 +0100

    Add POWER2

--- src/Power.cpp
+++ src/Power.cpp
@@ -9,11 +9,15 @@ void Power_Init(void) {
 	#if (POWER >= 0 && POWER <= MAX_GPIO)
 		pinMode(POWER, OUTPUT);     // Only necessary for GPIO. For port-expander it's done (previously) via Port_init()
 	#endif
+	#if (POWER2 >= 0 && POWER2 <= MAX_GPIO)
+		pinMode(POWER2, OUTPUT);     // Only necessary for GPIO. For port-expander it's done (previously) via Port_init()
+	#endif
 }
 
 // Switch on peripherals. Please note: meaning of POWER_ON is HIGH per default. But is LOW in case of INVERT_POWER is enabled.
 void Power_PeripheralOn(void) {
 	Port_Write(POWER, POWER_ON, false);
+	Port_Write(POWER2, POWER2_ON, false);
 	#ifdef BUTTONS_LED
 		Port_Write(BUTTONS_LED, HIGH, false);
 	#endif
@@ -23,6 +27,7 @@ void Power_PeripheralOn(void) {
 // Switch off peripherals. Please note: meaning of POWER_OFF is LOW per default. But is HIGH in case of INVERT_POWER is enabled.
 void Power_PeripheralOff(void) {
 	Port_Write(POWER, POWER_OFF, false);
+	Port_Write(POWER2, POWER2_OFF, false);
 	#ifdef BUTTONS_LED
 		Port_Write(BUTTONS_LED, LOW, false);
 	#endif
--- src/Power.h
+++ src/Power.h
@@ -7,6 +7,13 @@
 	#define POWER_ON HIGH
 	#define POWER_OFF LOW
 #endif
+#ifdef INVERT_POWER2
+	#define POWER2_ON LOW
+	#define POWER2_OFF HIGH
+#else
+	#define POWER2_ON HIGH
+	#define POWER2_OFF LOW
+#endif
 
 void Power_Init(void);
 void Power_PeripheralOn(void);
