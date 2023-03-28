86b52b3c8440a9fc4d83e59d3ac0f45da1714241
SciLor <SciLor@github.com>
2023-03-28 19:18:27 +0200

    Dac init partly works, sound is there

--- src/Dac.cpp
+++ src/Dac.cpp
@@ -1,15 +1,30 @@
 #include "Dac.h"
+#include "Log.h"
 
 #ifdef DAC_TLV320DAC3100_ENABLE
     RvX_TLV320DAC3100 dac = RvX_TLV320DAC3100();
 #endif
 void Dac_Init(void) {
     #ifdef DAC_TLV320DAC3100_ENABLE
+        const int resetDACPin = 26;
         const int i2c_sda = 5;
         const int i2c_scl = 6;
+
+        pinMode(resetDACPin, OUTPUT); 
+        delay(50); 
+        digitalWrite(resetDACPin, 0);
+        delay(10);
+        digitalWrite(resetDACPin, 1);
+        delay(10);
+
         Wire.setPins(i2c_sda, i2c_scl);
-        dac.begin(0x18);
-        dac.beep();
+        Wire.begin();
+        if (!dac.begin(0x18)) {
+			Log_Println((char *) F("DAC: NOT FOUND!"), LOGLEVEL_ERROR);
+        } else {
+			Log_Println((char *) F("DAC: FOUND!"), LOGLEVEL_INFO);
+        }
+        //dac.beep(); //hangs
     #endif
 }
 void Dac_Cyclic(void) {
