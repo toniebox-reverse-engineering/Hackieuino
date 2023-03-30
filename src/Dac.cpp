66e536828ef78114067504679fab17776d79fa68
SciLor <SciLor@github.com>
2023-03-30 20:51:31 +0200

    workaround for powerDown Dac

--- src/Dac.cpp
+++ src/Dac.cpp
@@ -3,18 +3,18 @@
 
 #ifdef DAC_TLV320DAC3100_ENABLE
     RvX_TLV320DAC3100 dac = RvX_TLV320DAC3100();
+	#define RESET_DAC_PIN 26
 #endif
 void Dac_Init(void) {
     #ifdef DAC_TLV320DAC3100_ENABLE
-        const int resetDACPin = 26;
         const int i2c_sda = 5;
         const int i2c_scl = 6;
 
-        pinMode(resetDACPin, OUTPUT); 
+        pinMode(RESET_DAC_PIN, OUTPUT); 
         delay(50); 
-        digitalWrite(resetDACPin, 0);
+        digitalWrite(RESET_DAC_PIN, 0);
         delay(10);
-        digitalWrite(resetDACPin, 1);
+        digitalWrite(RESET_DAC_PIN, 1);
         delay(10);
 
         Wire.setPins(i2c_sda, i2c_scl);
@@ -33,5 +33,6 @@ void Dac_Cyclic(void) {
 void Dac_Exit(void) {
     #ifdef DAC_TLV320DAC3100_ENABLE
         dac.powerDown();
+        digitalWrite(RESET_DAC_PIN, 0); //Workaround
     #endif
 }
