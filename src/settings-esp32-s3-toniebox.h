0894412caada38e30b69a955e3d9470c7327654e
SciLor <SciLor@github.com>
2023-04-02 16:57:26 +0200

    Add charger and battery

--- src/settings-esp32-s3-toniebox.h
+++ src/settings-esp32-s3-toniebox.h
@@ -42,7 +42,7 @@
     //#define HEADPHONE_ADJUST_ENABLE       // Used to adjust (lower) volume for optional headphone-pcb (refer maxVolumeSpeaker / maxVolumeHeadphone) and to enable stereo (if PLAY_MONO_SPEAKER is set)
     #define PLAY_MONO_SPEAKER               // If only one speaker is used enabling mono should make sense. Please note: headphones is always stereo (if HEADPHONE_ADJUST_ENABLE is active)
     #define SHUTDOWN_IF_SD_BOOT_FAILS       // Will put ESP to deepsleep if boot fails due to SD. Really recommend this if there's in battery-mode no other way to restart ESP! Interval adjustable via deepsleepTimeAfterBootFails.
-    //#define MEASURE_BATTERY_VOLTAGE         // Enables battery-measurement via GPIO (ADC) and voltage-divider
+    #define MEASURE_BATTERY_VOLTAGE         // Enables battery-measurement via GPIO (ADC) and voltage-divider
     //#define MEASURE_BATTERY_MAX17055      // Enables battery-measurement via external fuel gauge (MAX17055)
     //#define SHUTDOWN_ON_BAT_CRITICAL      // Whether to turn off on critical battery-level (only used if MEASURE_BATTERY_XXX is active)
     //#define PLAY_LAST_RFID_AFTER_REBOOT   // When restarting ESPuino, the last RFID that was active before, is recalled and played
@@ -249,15 +249,23 @@
 
     #if defined(MEASURE_BATTERY_VOLTAGE) || defined(MEASURE_BATTERY_MAX17055)
         #define BATTERY_MEASURE_ENABLE                 // Don't change. Set automatically if any method of battery monitoring is selected.
-        constexpr uint8_t s_batteryCheckInterval = 10; // How often battery is measured (in minutes) (can be changed via GUI!)
+        constexpr uint8_t s_batteryCheckInterval = 1; // How often battery is measured (in minutes) (can be changed via GUI!)
     #endif
 
     #ifdef MEASURE_BATTERY_VOLTAGE
         // (optional) Default-voltages for battery-monitoring via Neopixel; can be changed later via WebGUI
-        constexpr float s_warningLowVoltage = 3.4;                      // If battery-voltage is <= this value, a cyclic warning will be indicated by Neopixel (can be changed via GUI!)
-        constexpr float s_warningCriticalVoltage = 3.1;                 // If battery-voltage is <= this value, assume battery near-empty. Set to 0V to disable.
+        constexpr float s_warningLowVoltage = 3.6;                      // If battery-voltage is <= this value, a cyclic warning will be indicated by Neopixel (can be changed via GUI!)
+        constexpr float s_warningCriticalVoltage = 3.3;                 // If battery-voltage is <= this value, assume battery near-empty. Set to 0V to disable.
         constexpr float s_voltageIndicatorLow = 3.0;                    // Lower range for Neopixel-voltage-indication (0 leds) (can be changed via GUI!)
-        constexpr float s_voltageIndicatorHigh = 4.2;                   // Upper range for Neopixel-voltage-indication (all leds) (can be changed via GUI!)
+        constexpr float s_voltageIndicatorHigh = 4.5;                   // Upper range for Neopixel-voltage-indication (all leds) (can be changed via GUI!)
+    #endif
+    // (optional) Monitoring of battery-voltage via ADC
+    #ifdef MEASURE_BATTERY_VOLTAGE
+        #define VOLTAGE_READ_PIN            9          // GPIO used to monitor battery-voltage.
+        constexpr float referenceVoltage = 3.3;         // Voltage between 3.3V and GND-pin in battery-mode (disconnect USB!)
+        constexpr float offsetVoltage = 0.0;            // If voltage measured by ESP isn't 100% accurate, you can add an correction-value here
+		constexpr uint16_t rdiv1 = 100;			// Rdiv1 of voltage-divider (kOhms)
+		constexpr uint16_t rdiv2 = 33;			// Rdiv2 of voltage-divider (kOhms) => used to measure voltage via ADC!
     #endif
 
     #ifdef MEASURE_BATTERY_MAX17055
